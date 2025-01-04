4f5be2df7cdfafc0a8fa995d85ba2b37e9801877
ccjayanti <cc.jayantibalmuchu@gmail.com>
2025-01-04 14:13:27 +0530

    clear previous data and errors

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,31 @@
 "use client";
 
 import axios from "axios";
-import {useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
-  const [name, setName] = useState("");
-  const [verifiedStatus, setVerifiedStatus] = useState();
-  const [dateJoined, setDateJoined] = useState("");
-  const [countryName, setCountryName] = useState();
+  const [name, setName] = useState(null);
+  const [verifiedStatus, setVerifiedStatus] = useState(null);
+  const [dateJoined, setDateJoined] = useState(null);
+  const [countryName, setCountryName] = useState(null);
   const [followers, setFollowers] = useState();
-  const [title, setTitle] = useState();
-  const [league, setLeague] = useState();
+  const [title, setTitle] = useState(null);
+  const [league, setLeague] = useState(null);
+  const [error, setError] = useState(null);
 
   const fetchProfile = (username) => {
+
+    // Clear previous data and errors
+    setName("");
+    setVerifiedStatus(false);
+    setDateJoined("");
+    setCountryName("");
+    setFollowers("");
+    setTitle("");
+    setLeague("");
+    setError(null);
+
     axios
       .get(`https://api.chess.com/pub/player/${username}`)
       .then((response) => {
@@ -23,11 +35,9 @@ export default function Home() {
         setName(name);
         verified ? setVerifiedStatus(true) : setVerifiedStatus(false);
         setDateJoined(joined); // Update the joined date state
-
         setFollowers(followers); // Update follower Count 
         setTitle(title);
         setLeague(league);
-
 
         // Fetch country details
         return axios.get(country);
@@ -37,7 +47,8 @@ export default function Home() {
         setCountryName(name);
       })
       .catch((error) => {
-        console.error(error);
+        console.log(error);
+        setError("Enter a valid username");
       });
   };
 
@@ -50,20 +61,23 @@ export default function Home() {
 
   return (
     <div className="px-10 py-10">
-      <h1>chess.com</h1>
+      <h1 className=" font-bold text-xl ">chess.com Profile DashBoard</h1>
       <input
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         onKeyDown={handleKeyPress}
-        placeholder="Enter username"
+        placeholder="Enter a username"
         className="text-black outline-none bg-green-200"
       />
+      {error && <div>{error} </div>}
       {name && <div>Name: {name}{verifiedStatus && (<span>✅</span>)}</div>}
       {dateJoined && <div>Joined: {new Date(dateJoined * 1000).toUTCString()}</div>}
-      {countryName && <div>country: {countryName}</div>}
-      {followers && <div>followers: {followers} </div>}
+      {countryName && <div>Country: {countryName}</div>}
+      {followers >=1 && <div>Followers: {followers} </div>}
+
       {title && <div>Title: {title}</div>}
       {league && <div>League: {league}</div>}
+
     </div>
   );
 }
